bf33a968c76328901fefc4df6db6043b5f9175d9
sassystacks <rbeddoes@shaw.ca>
2017-01-04 18:08:54 -0700

    Add files via upload

---
+++ libraries/AP_Motors/AP_Motors.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "AP_Motors_Class.h"
+#include "AP_MotorsMulticopter.h"
+#include "AP_MotorsMatrix.h"
+#include "AP_MotorsTri.h"
+#include "AP_MotorsHeli_Single.h"
+#include "AP_MotorsSingle.h"
+#include "AP_MotorsCoax.h"
